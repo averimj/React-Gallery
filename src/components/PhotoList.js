5ac559ab6493f34173f0c0e6a23ff21caa01d66c
Averi Johnson <averimj@live.com>
2020-07-08 14:36:17 -0600

    add the url and key code

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import Photo from './Photo';
 
-const PhotoList = () => (
-  <div className="photo-container">
-    <h2>Results</h2>
-    <ul>
-      <li>
-        <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-      </li>
+const PhotoList = (props) => {
+
+  const results = props.data;
+  let photos = results.map(photo =>
+    <Photo url={photo.images.fixed_height.url} key={photos.id}/>
+  );
+
+  return(
+    <ul className='photo-list'>
+      {photos}
     </ul>
-  </div>
-);
+  );
+}
+
+// const PhotoList = () => (
+//   <div className="photo-container">
+//     <h2>Results</h2>
+//     <ul>
+//       <li>
+//         <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+//       </li>
+//       <li>
+//         <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
+//       </li>
+//       <li>
+//         <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
+//       </li>
+//       <li>
+//         <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
+//       </li>
+//     </ul>
+//   </div>
+// );
 
 export default PhotoList;
